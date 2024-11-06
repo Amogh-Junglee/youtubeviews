b58ce786857ca9fd447038b5dfefa78f36d1203a
Amogh, Bharadhwaj <amogh.bharadhwaj@jungleegames.com>
2024-11-06 11:17:21 +0530

    refactor: enhance context error handling and input validation in video service methods

--- service/service.go
+++ service/service.go
@@ -24,6 +24,10 @@ func CreateNewVideoService(redisClient *redis.Client, capacity int) *VideoServic
 }
 
 func (videoService *VideoService) Increment(ctx context.Context, videoId string) (views int, increment int, err error) {
+	if err := ctx.Err(); err != nil {
+        return -1,-1, fmt.Errorf("request cancelled or timed out: %v", err)
+    }
+
 	// Validate that the VideoID is provided
 	if videoId == "" {
 		return -1, -1, fmt.Errorf("videoId is required")
@@ -33,6 +37,10 @@ func (videoService *VideoService) Increment(ctx context.Context, videoId string)
 }
 
 func (videoService *VideoService) Get(ctx context.Context, videoId string) (views int, err error) {
+	if err := ctx.Err(); err != nil {
+        return -1, fmt.Errorf("request cancelled or timed out: %v", err)
+    }
+	// Validate that the VideoID is provided
 	if videoId == "" {
 		return -1, fmt.Errorf("videoId is required")
 	}
@@ -41,8 +49,14 @@ func (videoService *VideoService) Get(ctx context.Context, videoId string) (view
 }
 
 func (videoService *VideoService) GetTopVideos(ctx context.Context, page int, limit int) (topVideos []map[string]interface{}, err error) {
+	if err := ctx.Err(); err != nil {
+        return make([]map[string]interface{}, 0), fmt.Errorf("request cancelled or timed out: %v", err)
+    }
 	if page < 1 {
 		return make([]map[string]interface{}, 0), fmt.Errorf("page must be greater than 0")
+	}
+	if limit < 1 || limit > 100 {
+		return make([]map[string]interface{}, 0), fmt.Errorf("limit must be between 1 and 100")
 	}
 	return videoService.repo.GetTopVideos(ctx, page, limit)
 }
